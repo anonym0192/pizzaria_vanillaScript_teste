c663d4eae5a31e0029deab9d70a4d8f01e794d31
Anonimo192 <shakunin69@gmail.com>
2020-11-26 20:27:44 -0300

    'teste'

--- pizzas.js
+++ pizzas.js
@@ -9,25 +9,29 @@ let pizzaJson = [
 ];
 
 let newJson = [
-    {id:1, name:'Mussarela', img:'images/mussarela.png', price:[broto: 24.90, pequena: 35.90, media: 46.90, grande: 57.90], 
+    {id:1, name:'Mussarela', img:'images/mussarela.png', price:{broto: 24.90, pequena: 35.90, media: 46.90, grande: 57.90}, 
     description:'Molho, mussarela, tomate em rodelas e orégano.'},
     
     {id:2, name:'Calabresa', img:'images/calabresa.png', 
-    price:[broto: 24.90, pequena: 35.90, media: 46.90, grande: 57.90], 
+    price:{broto: 24.90, pequena: 35.90, media: 46.90, grande: 57.90}, 
     description:'Molho, mussarela, calabresa, cebola fatiada e orégano.'},
     
-    {id:3, name:'Portuguesa', img:'images/portuguesa.png', price:[broto: 25.90, pequena: 39.90, media: 56.90, grande: 67.90], 
+    {id:3, name:'Portuguesa', img:'images/portuguesa.png', price:{broto: 25.90, pequena: 39.90, media: 56.90, grande: 67.90}, 
     description:'Molho, mussarela, presunto, calabresa, ovos, cebola, azeitonas e orégano.'},
     
-    {id:4, name:'Chocolate', img:'images/chocolate.png', price:[broto: 24.90, pequena: 35.90, media: 46.90, grande: 57.90], 
+    {id:4, name:'Chocolate', img:'images/chocolate.png', price:{broto: 24.90, pequena: 35.90, media: 46.90, grande: 57.90}, 
     description:'Mussarela, chocolate ao leite.'},
     
-    {id:5, name:'5 Queijos', img:'images/cinco-queijos.png', price:[broto: 25.90, pequena: 39.90, media: 56.90, grande: 57.90], 
+    {id:5, name:'5 Queijos', img:'images/cinco-queijos.png', price:{broto: 25.90, pequena: 39.90, media: 56.90, grande: 57.90}, 
     description:'Molho, mussarela, provolone, gorgonzola, parmesão, catupiry e orégano.'},
     
-    {id:6, name:'Napolitana', img:'images/napolitana.png', price:[broto: 25.90, pequena: 39.90, media: 56.90, grande: 67.90], 
+    {id:6, name:'Napolitana', img:'images/napolitana.png', price:{broto: 25.90, pequena: 39.90, media: 56.90, grande: 67.90}, 
     description:'Molho, mussarela, tomates em rodelas, parmesão, azeitonas e orégano.'},
     
-    {id:7, name:'Calabresa', img:'images/calabresa.png',price:[broto: 24.90, pequena: 35.90, media: 46.90, grande: 57.90], 
+    {id:7, name:'Calabresa', img:'images/calabresa.png',price:{broto: 24.90, pequena: 35.90, media: 46.90, grande: 57.90}, 
     description:'Molho, mussarela, presunto, calabresa, ovos, cebola, azeitonas e orégano.'}
 ];
+
+
+
+
